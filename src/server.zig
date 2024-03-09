79bb640d44a9ca6369f220900b67c47581fd63e4
Kamil Tomsik <info@tomsik.cz>
2024-03-09 21:19:36 +0100

    hopefully fix stopping, add 5s timeout for keepalive connections

--- src/server.zig
+++ src/server.zig
@@ -17,9 +17,10 @@ pub const Server = struct {
     injector: Injector,
     net: std.net.Server,
     threads: []std.Thread,
+    mutex: std.Thread.Mutex = .{},
     stopping: std.Thread.ResetEvent = .{},
     stopped: std.Thread.ResetEvent = .{},
-    handler: *const fn (*ThreadContext) anyerror!void,
+    handler: *const fn (*Response, Injector) anyerror!void,
 
     /// Start a new server.
     pub fn start(allocator: std.mem.Allocator, handler: anytype, options: Options) !*Server {
@@ -61,71 +62,89 @@ pub const Server = struct {
     /// Stop and deinitialize the server.
     pub fn deinit(self: *Server) void {
         self.stopping.set();
-        defer self.stopped.set();
 
-        for (self.threads) |_| (std.net.tcpConnectToAddress(self.net.listen_address) catch continue).close();
-        for (self.threads) |t| t.join();
-
+        if (std.net.tcpConnectToAddress(self.net.listen_address)) |c| c.close() else |_| {}
         self.net.deinit();
+
+        for (self.threads) |t| t.join();
+        self.allocator.free(self.threads);
+
+        self.stopped.set();
         self.allocator.destroy(self);
     }
 
-    fn loop(self: *Server) !void {
-        var arena = std.heap.ArenaAllocator.init(self.allocator);
+    fn accept(self: *Server) ?std.net.Server.Connection {
+        self.mutex.lock();
+        defer self.mutex.unlock();
+
+        if (self.stopping.isSet()) return null;
+
+        return self.net.accept() catch |e| {
+            if (self.stopping.isSet()) return null;
+
+            // TODO: not sure what we can do here
+            //       but we should not just throw because that would crash the thread silently
+            std.debug.panic("accept: {}", .{e});
+        };
+    }
+
+    fn loop(server: *Server) void {
+        var arena = std.heap.ArenaAllocator.init(server.allocator);
         defer arena.deinit();
 
-        accept: while (!self.stopping.isSet()) {
-            const conn = try self.net.accept();
+        accept: while (server.accept()) |conn| {
             defer conn.stream.close();
+
+            const timeout = std.os.timeval{
+                .tv_sec = @as(i32, 5),
+                .tv_usec = @as(i32, 0),
+            };
+
+            std.os.setsockopt(conn.stream.handle, std.os.SOL.SOCKET, std.os.SO.RCVTIMEO, std.mem.asBytes(&timeout)) catch {};
 
             var buf: [10 * 1024]u8 = undefined;
             var http = std.http.Server.init(conn, &buf);
 
             while (http.state == .ready) {
-                _ = arena.reset(.{ .retain_with_limit = 8 * 1024 * 1024 });
+                defer _ = arena.reset(.{ .retain_with_limit = 8 * 1024 * 1024 });
 
-                var req = try Request.init(arena.allocator(), http.receiveHead() catch |e| {
+                const raw = http.receiveHead() catch |e| {
                     if (e != error.HttpConnectionClosing) log.err("receiveHead: {}", .{e});
                     continue :accept;
-                });
+                };
+
+                var req = Request.init(arena.allocator(), raw) catch {
+                    log.err("invalid uri {s}", .{raw.head.target});
+                    continue :accept;
+                };
+
                 var res = Response.init(&req);
 
-                var ctx = ThreadContext{
+                var ctx = .{
                     .allocator = req.allocator,
-                    .server = self,
+                    .server = server,
                     .req = &req,
                     .res = &res,
                 };
 
-                defer {
-                    if (!res.responded) res.noContent() catch {};
-                    res.out.?.end() catch {};
-                    log.debug("{s} {s} {}", .{ @tagName(req.method), req.raw.head.target, @intFromEnum(res.status) });
-                }
+                server.handler(&res, Injector.multi(&.{ Injector.from(&ctx), ctx.server.injector })) catch |e| {
+                    log.err("failed to send: {}", .{e});
+                    continue :accept;
+                };
 
-                try self.handler(&ctx);
+                if (!res.responded) res.noContent() catch {};
+                res.out.?.end() catch {};
+                log.debug("{s} {s} {}", .{ @tagName(req.method), req.raw.head.target, @intFromEnum(res.status) });
             }
         }
     }
 };
 
-const ThreadContext = struct {
-    allocator: std.mem.Allocator,
-    server: *Server,
-    req: *Request,
-    res: *Response,
-};
-
-fn trampoline(handler: anytype) fn (*ThreadContext) anyerror!void {
+fn trampoline(handler: anytype) fn (*Response, Injector) anyerror!void {
     const H = struct {
-        fn runInThread(ctx: *ThreadContext) !void {
-            const injector = Injector.multi(&.{ Injector.from(ctx), ctx.server.injector });
-
-            ctx.res.send(injector.call(handler, .{})) catch |e| {
-                log.debug("handleRequest: {}", .{e});
-                ctx.res.sendError(e) catch {};
-            };
+        fn handle(res: *Response, injector: Injector) !void {
+            return res.send(injector.call(handler, .{}));
         }
     };
-    return H.runInThread;
+    return H.handle;
 }
