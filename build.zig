a7bd6b732c96b182159c4635c79b4984d75bbaeb
Kamil Tomsik <info@tomsik.cz>
2024-04-26 19:05:02 +0200

    add TODO & error message

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const log = std.log.scoped(.tokamak);
 
 pub fn build(b: *std.Build) !void {
     const embed = b.option([]const []const u8, "embed", "Files to embed in the binary") orelse &.{};
@@ -15,12 +16,21 @@ pub fn build(b: *std.Build) !void {
     test_step.dependOn(&run_tests.step);
 }
 
+// TODO: This is simple and it works, it even recompiles if the files change.
+//       However, it's impossible to control when the files are read. So ie. it
+//       fails for `npm run build`
 fn embedFiles(b: *std.Build, root: *std.Build.Module, files: []const []const u8) !void {
     const options = b.addOptions();
     root.addOptions("embed", options);
 
     const contents = try b.allocator.alloc([]const u8, files.len);
     for (files, 0..) |path, i| {
+        errdefer |e| {
+            if (e == error.FileNotFound) {
+                log.err("File not found: {s}", .{path});
+            }
+        }
+
         contents[i] = try std.fs.cwd().readFileAlloc(
             b.allocator,
             path,
