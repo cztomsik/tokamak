dde3c36ee5f09c5ca5f846b40125d18c733cc0ad
Kamil Tomsik <info@tomsik.cz>
2024-05-31 22:44:56 +0200

    reorganize exports

--- src/main.zig
+++ src/main.zig
@@ -1,32 +1,21 @@
 const std = @import("std");
+const server = @import("server.zig");
 
 pub const config = @import("config.zig");
 pub const monitor = @import("monitor.zig").monitor;
 
 pub const Injector = @import("injector.zig").Injector;
-pub const Server = @import("server.zig").Server;
-pub const ServerOptions = @import("server.zig").Options;
-pub const Handler = @import("server.zig").Handler;
-pub const Context = @import("server.zig").Context;
+pub const Server = server.Server;
+pub const ServerOptions = server.Options;
+pub const Handler = server.Handler;
+pub const Context = server.Context;
 pub const Request = @import("request.zig").Request;
 pub const Params = @import("request.zig").Params;
 pub const Response = @import("response.zig").Response;
 
-pub const chain = @import("middleware.zig").chain;
-pub const group = @import("middleware.zig").group;
-pub const provide = @import("middleware.zig").provide;
-pub const send = @import("middleware.zig").send;
-pub const logger = @import("middleware.zig").logger;
-pub const cors = @import("middleware.zig").cors;
-
-pub const sendStatic = @import("static.zig").sendStatic;
-
-pub const router = @import("router.zig").router;
-pub const get = @import("router.zig").get;
-pub const post = @import("router.zig").post;
-pub const put = @import("router.zig").put;
-pub const patch = @import("router.zig").patch;
-pub const delete = @import("router.zig").delete;
+pub usingnamespace @import("router.zig");
+pub usingnamespace @import("middleware.zig");
+pub usingnamespace @import("static.zig");
 
 test {
     std.testing.refAllDecls(@This());
