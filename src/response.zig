f3049a2a74247537e4beecfbaeb172ee3d353a6e
Kamil Tomsik <info@tomsik.cz>
2024-06-06 07:54:53 +0200

    response: get rid of .responded, remove SSE temporarily, add res.writeHead(writer)

--- src/response.zig
+++ src/response.zig
@@ -1,32 +1,26 @@
 const std = @import("std");
 const Request = @import("request.zig").Request;
 
+/// Server response.
 pub const Response = struct {
-    req: *Request,
-    responded: bool = false,
-    keep_alive: bool = true,
-    sse: bool = false,
-    status: std.http.Status = .ok,
+    status: ?std.http.Status = null,
     headers: std.ArrayList(std.http.Header),
-    out: ?std.http.Server.Response = null,
-    buf: [1024]u8 = undefined,
+    body: union(enum) {
+        slice: []const u8,
+        stream: std.io.AnyReader,
+    } = .{ .slice = "" },
+    keep_alive: bool = false,
 
-    /// Sets a header. If the response has already been sent, this function
-    /// returns an error. Both `name` and `value` must be valid for the entire
+    /// Sets a header. Both `name` and `value` must be valid for the entire
     /// lifetime of the response.
     pub fn setHeader(self: *Response, name: []const u8, value: []const u8) !void {
-        if (self.responded) return error.HeadersAlreadySent;
-
         try self.headers.append(.{ .name = name, .value = value });
     }
 
-    /// Sets a cookie. If the response has already been sent, this function
-    /// returns an error.
+    /// Sets a cookie.
     pub fn setCookie(self: *Response, name: []const u8, value: []const u8, options: CookieOptions) !void {
-        if (self.responded) return error.HeadersAlreadySent;
-
         // TODO: start with current header?
-        var buf = std.ArrayList(u8).init(self.req.allocator);
+        var buf = std.ArrayList(u8).init(self.headers.allocator);
 
         try buf.appendSlice(name);
         try buf.append('=');
@@ -53,29 +47,24 @@ pub const Response = struct {
         try self.setHeader("Set-Cookie", buf.items);
     }
 
-    /// Returns a writer for the response body. If the response has already been
-    /// sent, this function returns an error.
-    pub fn writer(self: *Response) !std.io.AnyWriter {
-        if (!self.responded) try self.respond();
-        return self.out.?.writer();
-    }
-
     /// Sends a response depending on the type of the value.
     pub fn send(self: *Response, res: anytype) !void {
-        if (comptime @TypeOf(res) == []const u8) return self.sendChunk(res);
-
-        return switch (comptime @typeInfo(@TypeOf(res))) {
-            .Void => if (!self.responded) self.sendStatus(.no_content),
+        return switch (@typeInfo(@TypeOf(res))) {
+            .Void => if (self.status == null) { // tk.send() is void but sets status/body
+                self.status = .no_content;
+                self.body = .{ .slice = "" };
+            },
             .ErrorSet => self.sendError(res),
             .ErrorUnion => if (res) |r| self.send(r) else |e| self.sendError(e),
-            else => self.sendJson(res),
-        };
-    }
+            else => {
+                self.status = .ok;
 
-    /// Sends a status code.
-    pub fn sendStatus(self: *Response, status: std.http.Status) !void {
-        self.status = status;
-        try self.respond();
+                try switch (@TypeOf(res)) {
+                    []const u8 => self.sendText(res),
+                    else => self.sendJson(res),
+                };
+            },
+        };
     }
 
     /// Sends an error response.
@@ -91,73 +80,27 @@ pub const Response = struct {
         return self.sendJson(.{ .@"error" = err });
     }
 
-    /// Sends a chunk of data.
-    pub fn sendChunk(self: *Response, chunk: []const u8) !void {
-        var w = try self.writer();
-        try w.writeAll(chunk);
-        try self.out.?.flush();
+    /// Send text header/body.
+    pub fn sendText(self: *Response, body: []const u8) !void {
+        try self.setHeader("Content-Type", "text/plain");
+
+        self.body = .{ .slice = body };
     }
 
-    /// Sends a chunk of JSON. The chunk is always either single line, or in
-    /// case of SSE, it's a valid message ending with \n\n.
-    /// Supports values, slices and iterators.
+    /// Send JSON header/body.
     pub fn sendJson(self: *Response, body: anytype) !void {
-        if (!self.responded) {
-            try self.setHeader("Content-Type", "application/json");
-        }
+        try self.setHeader("Content-Type", "application/json");
 
-        var w = try self.writer();
-
-        if (self.sse) {
-            try w.writeAll("data: ");
-        }
-
-        if (comptime std.meta.hasFn(@TypeOf(body), "next")) {
-            var copy = body;
-            var i: usize = 0;
-
-            try w.writeAll("[");
-            while (try copy.next()) |item| : (i += 1) {
-                if (i != 0) try w.writeAll(",");
-                try std.json.stringify(item, .{}, w);
-            }
-            try w.writeAll("]");
-        } else {
-            try std.json.stringify(body, .{}, w);
-        }
-
-        try w.writeAll(if (self.sse) "\n\n" else "\r\n");
-        try self.out.?.flush();
-    }
-
-    /// Sends a chunk of JSON lines. The chunk always ends with a newline.
-    /// Expects an iterator.
-    pub fn sendJsonLines(self: *Response, iterator: anytype) !void {
-        if (!self.responded) {
-            try self.setHeader("Content-Type", "application/jsonlines");
-            try self.respond();
-        }
-
-        var copy = iterator;
-        while (try copy.next()) |item| {
-            try self.sendJson(item);
-        }
-    }
-
-    /// Starts an SSE stream.
-    pub fn startSse(self: *Response) !void {
-        try self.setHeader("Content-Type", "text/event-stream");
-        try self.setHeader("Cache-Control", "no-cache");
-        try self.setHeader("Connection", "keep-alive");
-
-        self.sse = true;
-        try self.respond();
+        self.body = .{
+            .slice = try std.json.stringifyAlloc(self.headers.allocator, body, .{}),
+        };
     }
 
     /// Redirects the response to a different URL.
     pub fn redirect(self: *Response, url: []const u8, options: struct { status: std.http.Status = .found }) !void {
+        self.status = options.status;
+
         try self.setHeader("Location", url);
-        try self.sendStatus(options.status);
     }
 
     /// Adds no-cache headers to the response.
@@ -167,19 +110,29 @@ pub const Response = struct {
         try self.setHeader("Expires", "0");
     }
 
-    /// Start the response manually.
-    pub fn respond(self: *Response) !void {
-        if (self.responded) return error.ResponseAlreadyStarted;
+    // Internal
+    pub fn writeHead(self: *Response, writer: anytype) !void {
+        const status = self.status orelse return error.InvalidState;
 
-        self.responded = true;
-        self.out = self.req.raw.respondStreaming(.{
-            .send_buffer = &self.buf,
-            .respond_options = .{
-                .status = self.status,
-                .keep_alive = self.keep_alive,
-                .extra_headers = self.headers.items,
-            },
+        try writer.print("HTTP/1.1 {d} {s}\r\n", .{
+            @intFromEnum(status),
+            status.phrase() orelse "",
         });
+
+        for (self.headers.items) |h| {
+            try writer.print("{s}: {s}\r\n", .{ h.name, h.value });
+        }
+
+        try switch (self.body) {
+            .slice => |data| writer.print("Content-Length: {d}\r\n", .{data.len}),
+            .stream => writer.print("Transfer-Encoding: chunked\r\n", .{}),
+        };
+
+        if (!self.keep_alive) {
+            try writer.print("Connection: close\r\n", .{});
+        }
+
+        try writer.print("\r\n", .{});
     }
 };
 
