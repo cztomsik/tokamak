52e6c42ce1c2bc003d3e97cfa006b0c9039d0f63
Kamil Tomsik <info@tomsik.cz>
2024-08-21 12:31:57 +0200

    example: add basic CRUD

--- example/src/main.zig
+++ example/src/main.zig
@@ -1,19 +1,11 @@
 const std = @import("std");
 const tk = @import("tokamak");
-
-const api = struct {
-    pub fn @"GET /"() []const u8 {
-        return "Hello";
-    }
-
-    pub fn @"GET /:name"(allocator: std.mem.Allocator, name: []const u8) ![]const u8 {
-        return std.fmt.allocPrint(allocator, "Hello {s}", .{name});
-    }
-};
+const api = @import("api.zig");
 
 const routes = &.{
     tk.logger(.{}, &.{
-        tk.get("/", tk.send("Hello index")),
+        tk.get("/", tk.sendStatic("public/index.html")),
+        tk.get("/main.js", tk.sendStatic("public/main.js")),
         tk.group("/api", &.{
             tk.router(api),
         }),
@@ -24,7 +16,15 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
-    var server = try tk.Server.init(gpa.allocator(), routes, .{});
+    var store = api.Store.init(gpa.allocator());
+    defer store.deinit();
+
+    try store.put(1, .{ .id = 1, .title = "Hello, World!", .body = "This is a test post." });
+    try store.put(2, .{ .id = 2, .title = "Goodbye, World!", .body = "This is another test post." });
+
+    var server = try tk.Server.init(gpa.allocator(), routes, .{
+        .injector = tk.Injector.init(&.{&store}, null),
+    });
     defer server.deinit();
 
     try server.listen(.{ .port = 8080 });
