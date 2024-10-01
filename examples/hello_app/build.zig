ba05cd8efeefba1dac65952656207d7a3c327e40
Kamil Tomsik <info@tomsik.cz>
2024-10-01 19:58:25 +0200

    examples: add hello_app

---
+++ examples/hello_app/build.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "hello_app",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("tokamak", b.dependency("tokamak", .{}).module("tokamak"));
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
---
+++ examples/hello_app/src/main.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+const tk = @import("tokamak");
+
+const App = struct {
+    server: *tk.Server,
+    routes: []const tk.Route = &.{
+        .get("/", hello),
+    },
+
+    fn hello() ![]const u8 {
+        return "Hello, world!";
+    }
+};
+
+pub fn main() !void {
+    try tk.run(App);
+}
