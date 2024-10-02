899e6bf5c4614932da2996a9992c1e80c3dcd723
Kamil Tomsik <info@tomsik.cz>
2024-10-02 06:59:52 +0200

    example: add note

--- examples/blog/src/api.zig
+++ examples/blog/src/api.zig
@@ -2,6 +2,10 @@ const std = @import("std");
 const Post = @import("model.zig").Post;
 const BlogService = @import("model.zig").BlogService;
 
+// NOTE: You can also pass the BlogService functions as handlers directly, but
+//       sometimes the signature does not match 1:1. Or you can skip the service
+//       layer entirely and put everything in this file.
+
 pub fn @"GET /posts"(svc: *BlogService, allocator: std.mem.Allocator) ![]const Post {
     return svc.getPosts(allocator);
 }
