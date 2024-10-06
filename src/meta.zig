149aa94cb383691436d75fb04626d755795e4780
Kamil Tomsik <info@tomsik.cz>
2024-10-06 17:23:40 +0200

    meta: add hadDecl() & dupe()

--- src/meta.zig
+++ src/meta.zig
@@ -15,6 +15,24 @@ pub inline fn tid(comptime T: type) TypeId {
     return &H.id;
 }
 
+pub fn dupe(allocator: std.mem.Allocator, value: anytype) !@TypeOf(value) {
+    std.log.debug("dupe {s}", .{@typeName(@TypeOf(value))});
+
+    return switch (@typeInfo(@TypeOf(value))) {
+        .optional => try dupe(allocator, value orelse return null),
+        .@"struct" => |s| {
+            var res: @TypeOf(value) = undefined;
+            inline for (s.fields) |f| @field(res, f.name) = try dupe(allocator, @field(value, f.name));
+            return res;
+        },
+        .pointer => |p| switch (p.size) {
+            .Slice => if (p.child == u8) allocator.dupe(p.child, value) else error.NotSupported,
+            else => value,
+        },
+        else => value,
+    };
+}
+
 pub fn Return(comptime fun: anytype) type {
     return switch (@typeInfo(@TypeOf(fun))) {
         .@"fn" => |f| f.return_type.?,
@@ -44,4 +62,11 @@ pub fn isOnePtr(comptime T: type) bool {
 
 pub fn Deref(comptime T: type) type {
     return if (isOnePtr(T)) std.meta.Child(T) else T;
+}
+
+pub fn hasDecl(comptime T: type, comptime name: []const u8) bool {
+    return switch (@typeInfo(T)) {
+        .@"struct", .@"union", .@"enum", .@"opaque" => @hasDecl(T, name),
+        else => false,
+    };
 }
