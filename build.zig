248c8e0b3ebc4dce9c7ee56d9e62921ffe1c199e
Kamil Tomsik <info@tomsik.cz>
2024-12-04 23:53:31 +0100

    fix tests?

--- build.zig
+++ build.zig
@@ -17,6 +17,7 @@ pub fn build(b: *std.Build) !void {
 
     const tests = b.addTest(.{ .root_source_file = b.path("src/main.zig") });
     tests.root_module.addImport("httpz", httpz.module("httpz"));
+    tests.root_module.link_libc = true;
     const run_tests = b.addRunArtifact(tests);
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_tests.step);
