14c303996c332af9691b8cac720bcffa1a571a5f
saicu <39388914+saicu@users.noreply.github.com>
2025-05-06 09:24:44 +0200

    fix examples ci (#18)
    * fix ci
    
    * exit after 15sec when server doesnt start
    
    * ci exit log fix
    
    * kill zig instead
    
    * pkill example instead
    
    * use kill again
    
    * start bin instead of using run
    
    * better log
    
    * try pid again

--- examples/blog/build.zig
+++ examples/blog/build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "example",
+        .name = "blog",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
