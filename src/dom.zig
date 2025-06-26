9dd5328147945e81c687e22d1adcc7b4ebdc662e
Kamil Tomsik <info@tomsik.cz>
2025-06-27 00:27:30 +0200

    dom: WIP continued

--- src/dom.zig
+++ src/dom.zig
@@ -7,6 +7,7 @@
 
 const std = @import("std");
 const sax = @import("sax.zig");
+const entities = @import("entities.zig");
 const Selector = @import("selector.zig").Selector;
 const QuerySelectorIterator = @import("selector.zig").QuerySelectorIterator;
 
@@ -40,9 +41,192 @@ pub const Node = struct {
         child.parent_node = self;
     }
 
+    pub fn visit(self: *Node, visitor: anytype) !void {
+        const Edge = union(enum) {
+            open: *Node,
+            close: *Node,
+        };
+
+        var next_edge: ?Edge = .{ .open = self };
+
+        while (next_edge) |edge| {
+            switch (edge) {
+                .open => |curr| {
+                    if (curr.element()) |el| try visitor.open(el);
+                    if (curr.text()) |tn| try visitor.text(tn);
+
+                    next_edge = if (curr.first_child) |child| .{ .open = child } else .{ .close = curr };
+                },
+                .close => |curr| {
+                    if (curr.element()) |el| try visitor.close(el);
+                    if (curr == self) break;
+
+                    next_edge = if (curr.next_sibling) |sib| .{ .open = sib } else .{ .close = curr.parent_node orelse break };
+                },
+            }
+        }
+    }
+
+    pub fn dump(self: *Node) void {
+        try self.visit(struct {
+            fn open(_: @This(), el: *Element) !void {
+                std.debug.print("open {s}\n", .{@tagName(el.local_name)});
+            }
+
+            fn close(_: @This(), el: *Element) !void {
+                std.debug.print("close {s}\n", .{@tagName(el.local_name)});
+            }
+
+            fn text(_: @This(), tn: *Text) !void {
+                std.debug.print("{s}\n", .{tn.data[0..@min(32, tn.data.len)]});
+            }
+        }{});
+    }
+
     fn downcast(self: *Node, comptime T: type) *T {
         std.debug.assert(self.kind == std.meta.fieldInfo(T, .node).defaultValue().?.kind);
         return @fieldParentPtr("node", self);
+    }
+};
+
+// TODO: packed union, Symbol?
+pub const LocalName = enum(u8) {
+    a,
+    abbr,
+    address,
+    area,
+    article,
+    aside,
+    audio,
+    b,
+    base,
+    bdi,
+    bdo,
+    blockquote,
+    body,
+    br,
+    button,
+    canvas,
+    caption,
+    cite,
+    code,
+    col,
+    colgroup,
+    data,
+    datalist,
+    dd,
+    del,
+    details,
+    dfn,
+    dialog,
+    div,
+    dl,
+    dt,
+    em,
+    embed,
+    fieldset,
+    figcaption,
+    figure,
+    footer,
+    form,
+    frame,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    head,
+    header,
+    hgroup,
+    hr,
+    html,
+    i,
+    iframe,
+    img,
+    input,
+    ins,
+    isindex,
+    kbd,
+    keygen,
+    label,
+    legend,
+    li,
+    link,
+    main,
+    map,
+    mark,
+    math,
+    menu,
+    meta,
+    meter,
+    nav,
+    noscript,
+    object,
+    ol,
+    optgroup,
+    option,
+    output,
+    p,
+    param,
+    picture,
+    pre,
+    progress,
+    q,
+    rp,
+    rt,
+    ruby,
+    s,
+    samp,
+    script,
+    search,
+    section,
+    select,
+    slot,
+    small,
+    source,
+    span,
+    strong,
+    style,
+    sub,
+    summary,
+    sup,
+    svg,
+    table,
+    tbody,
+    td,
+    template,
+    textarea,
+    tfoot,
+    th,
+    thead,
+    time,
+    title,
+    tr,
+    track,
+    u,
+    ul,
+    @"var",
+    video,
+    wbr,
+    unknown,
+
+    pub fn parse(name: []const u8) LocalName {
+        return std.meta.stringToEnum(LocalName, name) orelse .unknown;
+    }
+
+    pub fn isVoid(self: LocalName) bool {
+        return switch (self) {
+            .area, .base, .br, .col, .embed, .frame, .hr, .img, .input, .isindex, .keygen, .link, .meta, .param, .source, .track, .wbr => true,
+            else => false,
+        };
+    }
+
+    pub fn isRaw(self: LocalName) bool {
+        return switch (self) {
+            .script, .style => true,
+            else => false,
+        };
     }
 };
 
@@ -50,7 +234,7 @@ pub const Node = struct {
 // https://github.com/cztomsik/graffiti/blob/master/lib/core/Element.js
 pub const Element = struct {
     node: Node = .{ .kind = .element },
-    local_name: []const u8,
+    local_name: LocalName,
     attributes: std.BufMap,
 
     pub fn parentElement(self: *Element) ?*Element {
@@ -79,7 +263,7 @@ pub const Element = struct {
     }
 
     pub fn localName(self: *Element) []const u8 {
-        return self.local_name;
+        return @tagName(self.local_name);
     }
 
     pub fn id(self: *Element) []const u8 {
@@ -135,6 +319,8 @@ pub const Document = struct {
         allocator.destroy(self);
     }
 
+    // TODO: ParseOptions (so we can eagerly say that ie. we want to skip styles, scripts, ...)
+    //       and we can also put keep_whitespace there
     pub fn parseFromSlice(allocator: std.mem.Allocator, input: []const u8) !*Document {
         var parser = sax.Parser.initCompleteInput(input);
         return parseFromSax(allocator, &parser);
@@ -152,56 +338,83 @@ pub const Document = struct {
         const doc = try Document.init(allocator);
         errdefer doc.deinit();
 
-        var stack: std.ArrayList(*Node) = try .initCapacity(allocator, 64);
-        defer stack.deinit();
-
-        try stack.append(&doc.node);
-        parser.scanner.keep_whitespace = true;
+        // This is our "stack"
+        var top: *Node = &doc.node;
 
         var n_elem: usize = 0;
+        var n_attr: usize = 0;
         var n_text: usize = 0;
 
+        // TODO: ParseOptions
+        parser.scanner.keep_whitespace = true;
+
         while (try parser.next()) |ev| {
+            // std.debug.print("{s}", .{ev});
+
             switch (ev) {
                 .open => |tag| {
-                    if (stack.getLastOrNull()) |parent| {
-                        const el = try doc.createElement(tag);
-                        try stack.append(&el.node);
-                        parent.appendChild(&el.node);
-                        n_elem += 1;
+                    const el = try doc.createElement(tag);
+                    top.appendChild(&el.node);
+                    n_elem += 1;
+
+                    if (el.local_name.isRaw()) {
+                        // Skip everything until the next </xxx>
+                        while (try parser.next()) |ev2| {
+                            // NOTE: We MUST NOT use `tag` anymore!
+                            if (ev2 == .close and std.mem.eql(u8, ev2.close, el.localName())) break;
+                        }
+                    } else if (!el.local_name.isVoid()) {
+                        // Push to the top
+                        top = &el.node;
                     }
                 },
 
                 .attr => |att| {
-                    if (stack.getLastOrNull()) |parent| {
-                        if (parent.element()) |el| {
-                            try el.setAttribute(att.name, att.value);
-                        }
+                    if (top.element()) |el| {
+                        try el.setAttribute(att.name, att.value);
+                        n_attr += 1;
                     }
                 },
 
                 .text => |raw| {
-                    if (stack.getLastOrNull()) |parent| {
-                        // TODO: decode entities (in chunks); avoid double-cloning; maybe we could join too (later)
-                        const tn = try doc.createTextNode(raw);
-                        parent.appendChild(&tn.node);
-                        n_text += 1;
+                    if (top.element()) |el| {
+
+                        // TODO: avoid double-cloning, and maybe we could join too? (later)
+                        // TODO: and maybe the whole iterator api is nonsense too?
+                        var chunks = entities.Decoder(entities.html4).init(raw);
+                        while (chunks.next()) |chunk| {
+                            const tn = try doc.createTextNode(chunk);
+                            el.node.appendChild(&tn.node);
+                            n_text += 1;
+                        }
                     }
                 },
 
-                .close => {
-                    _ = stack.pop();
+                .close => |tag| {
+                    if (tag.len == 0) continue; // /> is always ignored in HTML
+                    const name = LocalName.parse(tag);
+
+                    // Pop everything up to the closest element in the stack (if any)
+                    var next = top.element();
+                    while (next) |el| : (next = el.parentElement()) {
+                        if (el.local_name == name) {
+                            top = el.node.parent_node orelse unreachable;
+                            break;
+                        }
+                    }
                 },
             }
         }
 
-        std.debug.print("mem used: {} stack: {}/{} #el: {} #text: {}\n", .{
+        std.debug.print("mem used: {} unclosed: {} #el: {} #attr: {} #text: {}\n", .{
             std.fmt.fmtIntSizeDec(doc.nodes.arena.queryCapacity()),
-            stack.items.len,
-            stack.capacity,
+            999, // TODO: count distance to the root
             n_elem,
+            n_attr,
             n_text,
         });
+
+        // doc.node.dump();
 
         return doc;
     }
@@ -212,7 +425,7 @@ pub const Document = struct {
 
         wrap.* = .{
             .element = .{
-                .local_name = try self.dupe(local_name),
+                .local_name = .parse(local_name),
                 .attributes = .init(self.arena()),
             },
         };
@@ -239,8 +452,15 @@ pub const Document = struct {
     }
 
     pub fn querySelectorAll(self: *Document, selector: []const u8) !QuerySelectorIterator(*Element) {
-        return .init(self.gpa(), selector, if (self.node.first_child) |node| node.element() else null);
+        return .init(self.gpa(), selector, self.documentElement(), null);
     }
+
+    pub fn documentElement(self: *Document) ?*Element {
+        return if (self.node.first_child) |node| node.element() else null;
+    }
+
+    // TODO: pub fn $() and $$()? (monad-like err-wrapping, fluent api, good-enough for simple modifications?)
+    // https://github.com/cheeriojs/cheerio https://api.jquery.com/ https://zeptojs.com/
 
     fn arena(self: *Document) std.mem.Allocator {
         return self.nodes.arena.allocator();
@@ -256,13 +476,13 @@ pub const Document = struct {
 };
 
 test {
-    var doc = try Document.parseFromSlice(std.testing.allocator, "<html><body><div class='btn'>Hello<br></div></body></html>");
+    var doc = try Document.parseFromSlice(std.testing.allocator, "<html lang='en'><body><div><button class='btn'>Hello <i>World</i></button><br></div></body></html>");
     defer doc.deinit();
 
-    try std.testing.expect(doc.node.first_child != null);
-    try std.testing.expect(doc.node.first_child.?.element() != null);
-    try std.testing.expectEqualStrings(doc.node.first_child.?.element().?.localName(), "html");
+    const html = doc.documentElement().?;
+    try std.testing.expectEqualStrings("html", html.localName());
+    try std.testing.expectEqualStrings("en", html.getAttribute("lang").?);
 
-    const el = try doc.querySelector("div.btn");
-    try std.testing.expect(el != null);
+    const btn = try doc.querySelector("div .btn");
+    try std.testing.expect(btn != null);
 }
