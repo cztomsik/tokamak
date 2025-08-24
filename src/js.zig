3156a34c620b6040ce1721171363c03923bb9e61
Kamil Tomsik <info@tomsik.cz>
2025-08-24 22:26:01 +0200

    js: WIP continued

--- src/js.zig
+++ src/js.zig
@@ -1,201 +1,62 @@
-// KISS, JS-like, stack-based VM. Let's don't worry about tokenization/parsing
-// for now, we can do that later.
+// Tiny, JS-like execution context.
 // TODO: Token, Tokenizer, Parser, Expr
 
 const std = @import("std");
 const meta = @import("meta.zig");
 const util = @import("util.zig");
+const VM = @import("vm.zig").VM;
+const Value = @import("vm.zig").Value;
 
-pub const Error = error{
-    TypeError,
-    UndefinedFunction,
-    UndefinedVar,
-    StackUnderflow,
-    OutOfMemory,
-    UnexpectedError,
-};
+pub const Context = struct {
+    vm: VM,
 
-const Ident = enum(u128) {
-    _,
+    pub fn init(gpa: std.mem.Allocator) !Context {
+        var vm = VM.init(gpa);
+        errdefer vm.deinit();
 
-    pub fn parse(ident: []const u8) Ident {
-        const x = util.Smol128.initShort(ident) orelse util.Smol128.initComptime("unknown");
-        return @enumFromInt(x.raw);
-    }
-
-    pub fn name(self: *const Ident) []const u8 {
-        return util.Smol128.str(@ptrCast(self));
-    }
-};
-
-const Value = union(enum) {
-    undefined,
-    bool: bool,
-    number: f64,
-    string: []const u8,
-    fun: Fun,
-    err: Error,
-
-    fn from(val: anytype) Value {
-        const T = @TypeOf(val);
-
-        if (T == Value) {
-            return val;
+        inline for (comptime std.meta.declarations(Builtins)) |d| {
+            try vm.define(d.name, @field(Builtins, d.name));
         }
 
-        if (T == []const Op) {
-            return .{ .fun = .{ .code = val } };
-        }
-
-        if (meta.isString(T)) {
-            return .{ .string = val };
-        }
-
-        if (meta.isSlice(T)) {
-            @compileError("TODO");
-        }
-
-        return switch (@typeInfo(T)) {
-            .void => .undefined,
-            .bool => .{ .bool = val },
-            .comptime_int, .int, .comptime_float, .float => .{ .number = val },
-            .error_union => if (val) |v| from(v) else |e| from(e),
-            .error_set => .{ .err = error.UnexpectedError },
-            .@"fn" => .{ .fun = .native(val) },
-            .pointer => from(val.*),
-            else => @compileError("TODO: Value.from " ++ @typeName(T)),
-        };
-    }
-
-    fn into(self: Value, comptime T: type) Error!T {
-        return switch (T) {
-            Value => self,
-            void => self.expect(.undefined),
-            bool => self.expect(.bool),
-            f64 => self.expect(.number),
-            []const u8 => self.expect(.string),
-            else => @compileError("TODO Value.into " ++ @typeName(T)),
-        };
-    }
-
-    fn expect(self: Value, comptime kind: std.meta.FieldEnum(Value)) !std.meta.fieldInfo(Value, kind).type {
-        return switch (self) {
-            kind => |t| t,
-            else => error.TypeError,
-        };
-    }
-};
-
-const Fun = union(enum) {
-    native_fn: *const fn (*VM) Error!Value,
-    code: []const Op,
-
-    fn native(fun: anytype) Fun {
-        const H = struct {
-            fn wrap(vm: *VM) Error!Value {
-                const Args = std.meta.ArgsTuple(@TypeOf(fun));
-                var args: Args = undefined;
-                const fields = std.meta.fields(Args);
-                inline for (fields, 0..) |field, i| {
-                    const val = vm.pop() orelse return error.TypeError;
-                    args[i] = try val.into(field.type);
-                }
-
-                const res = @call(.auto, fun, args);
-                return Value.from(res);
-            }
-        };
-
-        return .{ .native_fn = H.wrap };
-    }
-};
-
-const Op = union(enum) {
-    push: Value,
-    call: Ident,
-    load: Ident,
-    store: Ident,
-    pop,
-    dup,
-};
-
-const VM = struct {
-    gpa: std.mem.Allocator,
-    stack: std.ArrayList(Value),
-    env: std.AutoHashMap(Ident, Value),
-
-    pub fn init(gpa: std.mem.Allocator) Error!VM {
-        var self = initEmpty(gpa);
-        try Builtins.defineAll(&self);
-        return self;
-    }
-
-    pub fn initEmpty(gpa: std.mem.Allocator) VM {
         return .{
-            .gpa = gpa,
-            .stack = .init(gpa),
-            .env = .init(gpa),
+            .vm = vm,
         };
     }
 
-    pub fn deinit(self: *VM) void {
-        self.stack.deinit();
-        self.env.deinit();
+    pub fn deinit(self: *Context) void {
+        self.vm.deinit();
     }
 
-    pub fn define(self: *VM, name: []const u8, val: anytype) Error!void {
-        try self.env.put(Ident.parse(name), Value.from(val));
+    pub fn eval(self: *Context, expr: []const u8) !Value {
+        _ = self; // autofix
+        var tokenizer: Tokenizer = .{ .input = expr };
+        const tok = tokenizer.next() orelse @panic("TODO");
+
+        return switch (tok) {
+            .number => |num| .{ .number = num },
+            else => @panic("TODO"),
+        };
     }
 
-    pub fn push(self: *VM, val: anytype) Error!void {
-        return self.stack.append(Value.from(val));
-    }
+    pub fn print(self: *Context, writer: std.io.AnyWriter, val: Value) !void {
+        _ = self; // autofix
 
-    pub fn pop(self: *VM) ?Value {
-        return self.stack.pop();
-    }
-
-    pub fn eval(self: *VM, code: []const Op) Error!Value {
-        for (code) |op| {
-            switch (op) {
-                .push => |val| try self.push(val),
-                .call => |ident| {
-                    const res = try self.call(ident.name());
-                    try self.push(res);
-                },
-                .load => |ident| {
-                    const val = self.env.get(ident) orelse return Error.UndefinedVar;
-                    try self.push(val);
-                },
-                .store => |ident| {
-                    const val = self.pop() orelse return Error.StackUnderflow;
-                    try self.env.put(ident, val);
-                },
-                .pop => {
-                    _ = self.pop() orelse return Error.StackUnderflow;
-                },
-                .dup => {
-                    const val = self.pop() orelse return Error.StackUnderflow;
-                    try self.push(val);
-                    try self.push(val);
-                },
-            }
-        }
-
-        return self.pop() orelse .undefined;
-    }
-
-    pub fn call(self: *VM, fn_name: []const u8) Error!Value {
-        if (self.env.get(Ident.parse(fn_name))) |f| {
-            return switch (f.fun) {
-                .native_fn => |fun| fun(self),
-                .code => |ops| self.eval(ops),
-            };
-        }
-
-        return Error.UndefinedFunction;
+        // TODO: move SOME of this to Value.fmt/debug() but js.print() should probably stay JS-specific
+        try switch (val) {
+            .undefined => writer.print("undefined", .{}),
+            .null => writer.print("null", .{}),
+            .bool => |b| writer.print("{}", .{b}),
+            .number => |n| writer.print("{d}", .{n}),
+            .string => |s| writer.print("{s}", .{s}),
+            .fun => writer.print("[function]", .{}),
+            .err => |e| writer.print("error: {s}", .{@errorName(e)}),
+        };
     }
 };
+
+fn cx(vm: *VM) *Context {
+    return @fieldParentPtr("vm", vm);
+}
 
 const Builtins = struct {
     pub fn @"+"(a: f64, b: f64) f64 {
@@ -214,53 +75,69 @@ const Builtins = struct {
         return a / b;
     }
 
-    pub fn print(val: Value) void {
-        switch (val) {
-            .undefined => std.debug.print("undefined\n", .{}),
-            .bool => |b| std.debug.print("{}\n", .{b}),
-            .number => |n| std.debug.print("{}\n", .{n}),
-            .string => |s| std.debug.print("{s}\n", .{s}),
-            .fun => std.debug.print("[function]\n", .{}),
-            .err => |e| std.debug.print("error: {s}", .{@errorName(e)}),
-        }
+    pub fn print(vm: *VM, val: Value) !void {
+        const w = std.io.getStdErr().writer().any();
+        try cx(vm).print(w, val);
+        try w.writeByte('\n');
     }
 
-    pub fn eval(vm: *VM, code: []const Op) Error!Value {
-        return vm.eval(code);
-    }
-
-    fn defineAll(vm: *VM) Error!void {
-        inline for (comptime std.meta.declarations(@This())) |d| {
-            // TODO: Fix error union and then we can export this too?
-            if (comptime std.mem.eql(u8, d.name, "eval")) continue;
-            try vm.define(d.name, @field(@This(), d.name));
-        }
+    pub fn eval(vm: *VM, expr: []const u8) !Value {
+        return cx(vm).eval(expr);
     }
 };
 
-test VM {
-    var js = try VM.init(std.testing.allocator);
-    defer js.deinit();
+const Token = union(enum) {
+    ident: []const u8,
+    number: f64, // TODO: keep it []const u8?
+    lparen,
+    rparen,
+};
 
-    try js.push(1);
-    try js.push(2.5);
+const Tokenizer = struct {
+    input: []const u8,
+    pos: usize = 0,
 
-    const res = try js.call("+");
-    try std.testing.expectEqual(3.5, res.number);
+    fn next(self: *Tokenizer) ?Token {
+        if (self.pos >= self.input.len) return null;
+        const ch: u8 = self.input[self.pos];
+        self.pos += 1;
+
+        return switch (ch) {
+            '0'...'9' => .{ .number = std.fmt.parseFloat(f64, self.input) catch @panic("TODO") },
+            else => @panic("TODO"),
+        };
+    }
+};
+
+fn expectEval(js: *Context, expr: []const u8, expected: []const u8) !void {
+    var buf = std.ArrayList(u8).init(std.testing.allocator);
+    defer buf.deinit();
+
+    const res = try js.eval(expr);
+    try js.print(buf.writer().any(), res);
+
+    return std.testing.expectEqualStrings(expected, buf.items);
 }
 
-test "VM.eval()" {
-    var js = try VM.init(std.testing.allocator);
+test {
+    var js = try Context.init(std.testing.allocator);
     defer js.deinit();
 
-    const ops: []const Op = &.{
-        .{ .push = Value.from(5.0) },
-        .{ .push = Value.from(3.0) },
-        .{ .call = Ident.parse("+") },
-        .{ .push = Value.from(2.0) },
-        .{ .call = Ident.parse("*") },
-    };
+    // const add = js.vm.get("+");
+    // const res = try js.vm.call(add, &.{ Value.from(1), Value.from(2) });
+    try js.vm.push(Value.from(1));
+    try js.vm.push(Value.from(2));
+    const res = try js.vm.call("+");
 
-    const result = try js.eval(ops);
-    try std.testing.expectEqual(16.0, result.number);
+    try std.testing.expectEqual(3.0, res.number);
+
+    const res2 = try js.eval("123");
+    try std.testing.expectEqual(123.0, res2.number);
+
+    // try expectEval(&js, "", "undefined");
+    // try expectEval(&js, "null", "null");
+    // try expectEval(&js, "true", "true");
+    try expectEval(&js, "1", "1");
+    try expectEval(&js, "1.2", "1.2");
+    // try expectEval(&js, "\"foo\"", "\"foo\"");
 }
---
+++ src/vm.zig
@@ -0,0 +1,225 @@
+// Simple, general-purpose stack-based VM.
+
+const std = @import("std");
+const meta = @import("meta.zig");
+const util = @import("util.zig");
+
+pub const Error = error{
+    TypeError,
+    UndefinedFunction,
+    UndefinedVar,
+    StackUnderflow,
+    OutOfMemory,
+    UnexpectedError,
+};
+
+pub const Ident = enum(u128) {
+    _,
+
+    pub fn parse(ident: []const u8) Ident {
+        const x = util.Smol128.initShort(ident) orelse util.Smol128.initComptime("unknown");
+        return @enumFromInt(x.raw);
+    }
+
+    pub fn name(self: *const Ident) []const u8 {
+        return util.Smol128.str(@ptrCast(self));
+    }
+};
+
+// TODO: NaN boxing
+pub const Value = union(enum) {
+    undefined,
+    null,
+    bool: bool,
+    number: f64,
+    string: []const u8, // TODO: this should be owned
+    fun: Fun,
+    err: Error,
+    // TODO: arrays/slices
+
+    fn expect(self: Value, comptime kind: std.meta.FieldEnum(Value)) !std.meta.fieldInfo(Value, kind).type {
+        return switch (self) {
+            kind => |t| t,
+            else => error.TypeError,
+        };
+    }
+
+    pub fn from(val: anytype) Value {
+        const T = @TypeOf(val);
+
+        if (T == Value) return val;
+        if (meta.isString(T)) return .{ .string = val };
+        if (meta.isOptional(T)) return if (val) |v| from(v) else .null;
+        if (meta.isSlice(T)) @compileError("TODO");
+
+        return switch (@typeInfo(T)) {
+            .void => .undefined,
+            .null => .null,
+            .bool => .{ .bool = val },
+            .comptime_int, .int => .{ .number = @floatFromInt(val) },
+            .comptime_float, .float => .{ .number = @floatCast(val) },
+            .error_union => if (val) |v| from(v) else |e| from(e),
+            .error_set => .{ .err = error.UnexpectedError },
+            .@"fn" => .{ .fun = .wrap(val) },
+            .pointer => from(val.*),
+            else => @compileError("TODO: Value.from " ++ @typeName(T)),
+        };
+    }
+
+    pub fn into(self: Value, comptime T: type) Error!T {
+        return switch (T) {
+            Value => self,
+            void => self.expect(.undefined),
+            bool => self.expect(.bool),
+            f64 => self.expect(.number),
+            []const u8 => self.expect(.string),
+            else => @compileError("TODO Value.into " ++ @typeName(T)),
+        };
+    }
+};
+
+pub const Fun = union(enum) {
+    native: *const fn (*VM) Error!Value,
+    compiled: struct {
+        arity: u8,
+        code: []const Op,
+    },
+
+    fn wrap(fun: anytype) Fun {
+        const H = struct {
+            fn wrap(vm: *VM) Error!Value {
+                const Args = std.meta.ArgsTuple(@TypeOf(fun));
+                var args: Args = undefined;
+
+                inline for (std.meta.fields(Args), 0..) |f, i| {
+                    if (f.type == *VM) {
+                        args[i] = vm;
+                        continue;
+                    }
+
+                    const val = vm.pop() orelse return error.TypeError;
+                    args[i] = try val.into(f.type);
+                }
+
+                const res = @call(.auto, fun, args);
+                return Value.from(res);
+            }
+        };
+
+        return .{ .native = H.wrap };
+    }
+};
+
+pub const Op = union(enum) {
+    push: Value,
+    store: Ident,
+    load: Ident,
+    call: Ident, // TODO: argless?
+    pop,
+    dup,
+};
+
+pub const VM = struct {
+    gpa: std.mem.Allocator,
+    stack: std.ArrayList(Value),
+    env: std.AutoHashMap(Ident, Value),
+
+    pub fn init(gpa: std.mem.Allocator) VM {
+        return .{
+            .gpa = gpa,
+            .stack = .init(gpa),
+            .env = .init(gpa),
+        };
+    }
+
+    pub fn deinit(self: *VM) void {
+        self.stack.deinit();
+        self.env.deinit();
+    }
+
+    pub fn define(self: *VM, name: []const u8, val: anytype) Error!void {
+        try self.env.put(Ident.parse(name), Value.from(val));
+    }
+
+    pub fn get(self: *VM, name: []const u8) ?Value {
+        return self.env.get(Ident.parse(name));
+    }
+
+    pub fn push(self: *VM, val: anytype) Error!void {
+        return self.stack.append(Value.from(val));
+    }
+
+    pub fn pop(self: *VM) ?Value {
+        return self.stack.pop();
+    }
+
+    pub fn eval(self: *VM, code: []const Op) Error!Value {
+        for (code) |op| {
+            switch (op) {
+                .push => |val| try self.push(val),
+                .call => |ident| {
+                    const res = try self.call(ident.name());
+                    try self.push(res);
+                },
+                .load => |ident| {
+                    const val = self.env.get(ident) orelse return Error.UndefinedVar;
+                    try self.push(val);
+                },
+                .store => |ident| {
+                    const val = self.pop() orelse return Error.StackUnderflow;
+                    try self.env.put(ident, val);
+                },
+                .pop => {
+                    _ = self.pop() orelse return Error.StackUnderflow;
+                },
+                .dup => {
+                    const val = self.pop() orelse return Error.StackUnderflow;
+                    try self.push(val);
+                    try self.push(val);
+                },
+            }
+        }
+
+        return self.pop() orelse .undefined;
+    }
+
+    pub fn call(self: *VM, fn_name: []const u8) Error!Value {
+        if (self.env.get(Ident.parse(fn_name))) |f| {
+            return switch (f.fun) {
+                .native => |fun| fun(self),
+                .compiled => |fun| if (self.stack.items.len > fun.arity) self.eval(fun.code) else error.StackUnderflow,
+            };
+        }
+
+        return Error.UndefinedFunction;
+    }
+};
+
+test {
+    var vm = VM.init(std.testing.allocator);
+    defer vm.deinit();
+
+    try vm.push(1);
+    try vm.push(2.5);
+
+    const H = struct {
+        fn add(a: f64, b: f64) f64 {
+            return a + b;
+        }
+    };
+    try vm.define("+", H.add);
+
+    const res = try vm.call("+");
+    try std.testing.expectEqual(3.5, res.number);
+
+    const ops: []const Op = &.{
+        .{ .push = Value.from(5.0) },
+        .{ .push = Value.from(3.0) },
+        .{ .call = Ident.parse("+") },
+        .{ .push = Value.from(2.0) },
+        .{ .call = Ident.parse("+") },
+    };
+
+    const result = try vm.eval(ops);
+    try std.testing.expectEqual(10.0, result.number);
+}
