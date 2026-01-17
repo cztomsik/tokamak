187b3a3439d1b4e09dbefe099dbeb139dc6bbdbb
Kamil Tomsik <info@tomsik.cz>
2026-01-17 19:52:18 +0100

    docs: extend parser shape to 3-arg so that we can implement txt() properly

--- docs/zig-parser.js
+++ docs/zig-parser.js
@@ -1,5 +1,5 @@
-// Minimal Zig parser for the purpose of generating API documentation
-// by Kamil Tomsik (cztomsik)
+// Minimal Zig parser for the purpose of generating https://tomsik.cz/tokamak/api/
+// by Kamil Tomsik (cztomsik)   License: MIT
 const RE = /\/\/\/ ?(.*)|\/\/! ?(.*)|\/\/ ?(.*)|("(?:[^"\\]|\\.)*")|('(?:[^'\\]|\\.)*')|\b(catch|comptime|const|enum|error|fn|inline|opaque|pub|struct|test|try|union|var)\b|(@"(?:[^"\\\n]|\\.)*"|@\w+|[a-zA-Z_]\w*)|(\d[\w.]*)|([(){}[\],;:=.?*!<>@&|^~+\-/%])|[ \t\r\n]+|./gy
 const T = ['doc', 'cdoc', '//', 'str', 'ch', 1, 'id', 'num', 1]
 const C = ['struct', 'enum', 'union', 'opaque']
@@ -22,25 +22,25 @@ function parse(src) {
   const toks = tokenize(src);
   let r // shared & re-used
 
-  const t = k => i => toks[i]?.[1] === k && [i + 1, toks[i][2]]
-  const any = i => i < toks.length && [i + 1, toks[i][1]]
-  const any_of = ks => i => ks.includes(toks[i]?.[1]) && [i + 1, toks[i][1]]
-  const not = p => i => !p(i) && [i + 1, toks[i][1]]
+  const t = k => i => toks[i]?.[1] === k && [i + 1, 1, toks[i][2]]
+  const any = i => i < toks.length && [i + 1, 1, toks[i][1]]
+  const any_of = ks => i => ks.includes(toks[i]?.[1]) && [i + 1, 1, toks[i][1]]
+  const not = p => i => !p(i) && [i + 1, 1, toks[i][1]]
   const or = (...ps) => i => ps.reduce((r, p) => r || p(i), null)
-  const opt = p => i => p(i) || [i, null]
-  const map = (p, f) => i => (r = p(i)) && [r[0], f(r[1], i)]
-  const txt = p => i => (r = p(i)) && [r[0], src.slice(toks[i][0], toks[r[0]]?.[0]).trim()]
-  const seq = (...ps) => (i, a = []) => ps.every((p) => (r = p(i)) && ((i = r[0]), a.push(r[1]))) && [i, a]
-  const skip_until = p => (i, d = toks[i][3]) => {
+  const opt = p => i => p(i) || [i, 0, null]
+  const map = (p, f) => i => (r = p(i)) && [r[0], r[1], f(r[2], i)]
+  const txt = p => i => (r = p(i)) && [r[0], r[1], src.slice(toks[i][0], toks[i + r[1] - 1][0] + toks[i + r[1] - 1][2].length)]
+  const seq = (...ps) => (i, n = 0, a = []) => ps.every((p) => (r = p(i)) && ((i = r[0]), n += r[1], a.push(r[2]))) && [i, n, a]
+  const skip_until = p => (i, s = i, d = toks[i][3]) => {
     while (i < toks.length && !((r = p(i)) && toks[i]?.[3] === d)) i++
-    return [i, null]
+    return [i, i - s, null]
   }
-  const rep = (min, max, p, sep = null) => (i, arr = []) => {
+  const rep = (min, max, p, sep = null) => (i, n = 0, arr = []) => {
     while (arr.length < max && (r = p(i))) {
-      (i = r[0]), arr.push(r[1])
+      i = r[0], n += r[1], arr.push(r[2])
       if (sep) if (r = sep(i)) i = r[0]; else break
     }
-    return arr.length >= min && [i, arr]
+    return arr.length >= min && [i, n, arr]
   }
 
   const warn = i => i < toks.length && toks[i][1] !== '}' && [i+1, console.log('unmatched', toks[i], JSON.stringify(src.substr(toks[i][0], 32)))]
@@ -62,12 +62,12 @@ function parse(src) {
   const container = seq(any_of(C), cparams, t('{'), cfields, i => items(i), t('}'))
   const pub_const = seq(t('pub'), t('const'), id, t('='), container, t(';'))
   const api_type = map(pub_const, ([,,name,,[kind,params,,fields,children]], i) => ({ kind, params, name, fields, doc: toks[i][4], children }))
-  const api_export = seq(t('pub'), skip_decl) // TODO: { kind: "export" } + update build_docs
+  const api_export = map(seq(t('pub'), any_of(['const', 'var']), id, t('='), txt(seq(skip_until(t(';')), t(';')))), ([,, name,, value], i) => ({ kind: 'export', name, value, doc: toks[i][4] }))
 
   const item = or(api_fn, skip_fn, api_type, api_export, skip_decl, skip_test, skip_comptime, warn) // TODO: generics (pub fn(...) type)
   const items = map(rep(0, 999, item), rs => rs.filter(r => r?.kind))
 
-  return (r = items(0)) && r[1]
+  return (r = items(0)) && r[2]
 }
 
 export { parse, tokenize };
